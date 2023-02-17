8531f7bb5cd44c634e9ea45c2ae8efabc7d7f490
Harry <tharry@yandex.ru>
2023-02-18 00:04:31 +0300

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+
+  speed: 400,
+  spaceBetween: 20,
+
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  slidesPerView: 4,
+});
