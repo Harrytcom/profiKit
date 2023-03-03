3f14eb97bff08d2e3fd70712566ae7ea733d1da1
Harry <tharry@yandex.ru>
2023-03-03 22:58:29 +0300

    +card__footer eventListeners

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ const swiper = new Swiper('.product-slider__inner', {
 
   speed: 400,
   spaceBetween: 20,
-
+  
   direction: 'horizontal',
   loop: true,
 
@@ -16,5 +16,31 @@ const swiper = new Swiper('.product-slider__inner', {
     clickable: true,
   },
 
-  slidesPerView: 4,
-});
+  slidesPerView: 'auto',
+
+  breakpoints: {
+    1570: {
+      slidesPerView: 4,
+    },
+  }
+})
+
+const footers = document.querySelectorAll('.card__footer-js')
+
+footers.forEach(footer => {
+  const input = footer.querySelector('.stepper__input-js')
+
+  footer.addEventListener('click', function (event) {
+    if (event.target.classList.contains('stepper__arrow--up-js')) {
+      input.value++
+    } else if (event.target.classList.contains('stepper__arrow--down-js')) {
+      input.value = +input.value > 0 ? +input.value - 1 : 0
+    }
+  })
+  
+  footer.addEventListener('click', function (event) {
+    if (event.target.classList.contains('card__button')) {
+      input.value++
+    }
+  })
+})
